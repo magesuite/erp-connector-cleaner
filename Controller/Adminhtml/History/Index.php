827a8a45b95944163b48afea9a8738194a376b52
Piotr Matras <piotr.matras@creativestyle.pl>
2022-05-10 17:21:09 +0200

    feat: IPET-1114 admin view for connector cleaner

---
+++ Controller/Adminhtml/History/Index.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace MageSuite\ErpConnectorCleaner\Controller\Adminhtml\History;
+
+class Index extends \Magento\Backend\App\Action implements \Magento\Framework\App\Action\HttpGetActionInterface
+{
+    const ADMIN_RESOURCE = 'MageSuite_ErpConnectorCleaner::history';
+
+    public function execute()
+    {
+        $resultPage = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_PAGE);
+        $title = __('Clean Up History Summary');
+        $resultPage->setActiveMenu('MageSuite_ErpConnectorCleaner::clean_up_history');
+        $resultPage->addBreadcrumb($title, $title);
+        $resultPage->getConfig()->getTitle()->prepend($title);
+
+        return $resultPage;
+    }
+}
